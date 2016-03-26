98647a5a24a70dec8d6d93d4e89ba1f7136d0eb1
Thomas <Thomas@Thomas-PC>
2016-03-26 17:47:35 -0300

    attttt

--- GCMU/src/GCMU/UI/Controllers/CadastroDocenteController.java
+++ GCMU/src/GCMU/UI/Controllers/CadastroDocenteController.java
@@ -20,72 +20,66 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class CadastroDocenteController implements Initializable{
-	
-	@FXML
-	private TextField Nome_Field;
-	
-	@FXML
-	private TextField Suap_Field;
-	
-	@FXML
-	private TextField Cpf_Field;
-	
-	@FXML
-	private TextField Cargo_Field;
-	
-	@FXML
-	private Button Bt_CadastrarDocente;
-	
-	@FXML
-	private Button Bt_Voltar;
-	
-	@FXML
-	private void bCadastrarDocente() throws IOException{
-		
-		
-		Parent root = FXMLLoader.load(getClass().getResource("/GCMU/UI/Fxml/Crud.fxml"));
-		
-		Scene menu = new Scene(root);
-		Main.primaryStage.setTitle("Menu");
-		Main.primaryStage.setScene(menu);
-		Main.primaryStage.show(); 
-	
-	try {
-		Docente d = new Docente();
-		
-		d.setName(Nome_Field.getText());
-		d.setSiap(Integer.parseInt(Suap_Field.getText()));
-		d.setCpf(Cpf_Field.getText());
-		d.setCargo(Cargo_Field.getText());
-		d.insert(d);
-		
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-}
+public class CadastroDocenteController implements Initializable {
 
 	@FXML
-	private void btVoltar() throws IOException{
-		
-		
-		Parent root = FXMLLoader.load(getClass().getResource("/GCMU/UI/Fxml/Crud.fxml"));
-		
+	private TextField Nome_Field;
+
+	@FXML
+	private TextField Suap_Field;
+
+	@FXML
+	private TextField Cpf_Field;
+
+	@FXML
+	private TextField Cargo_Field;
+
+	@FXML
+	private Button Bt_Cadastrar;
+
+	@FXML
+	private Button Bt_Voltar;
+
+	@FXML
+	private void btCadastrar() throws IOException {
+
+		Parent root = FXMLLoader.load(getClass().getResource("/GCMU/UI/Fxml/Menu.fxml"));
+
 		Scene menu = new Scene(root);
 		Main.primaryStage.setTitle("Menu");
 		Main.primaryStage.setScene(menu);
-		Main.primaryStage.show(); 
+		Main.primaryStage.show();
+
+		try {
+			Docente d = new Docente();
+
+			d.setName(Nome_Field.getText());
+			d.setSiap(Integer.parseInt(Suap_Field.getText()));
+			d.setCpf(Cpf_Field.getText());
+			d.setCargo(Cargo_Field.getText());
+			d.insert(d);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	
+
+	@FXML
+	private void btVoltar() throws IOException {
+
+		Parent root = FXMLLoader.load(getClass().getResource("/GCMU/UI/Fxml/Crud.fxml"));
+
+		Scene menu = new Scene(root);
+		Main.primaryStage.setTitle("Menu");
+		Main.primaryStage.setScene(menu);
+		Main.primaryStage.show();
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
-	}
-	
 
-	
+	}
 
 }
